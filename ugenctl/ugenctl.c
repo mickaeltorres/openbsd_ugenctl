289822181916ed322ab0b2bf1f5bde56896f0c09
mike <mike@miky.me>
2015-12-07 00:06:50 +0100

    automatically reattach the device if connected

--- ugenctl/ugenctl.c
+++ ugenctl/ugenctl.c
@@ -6,6 +6,14 @@
 #include <err.h>
 #include <dev/usb/usb.h>
 
+struct usb_dev_list {
+        u_short vid;
+        u_short pid;
+        u_char addr;
+        char *ctrl;
+        struct usb_dev_list *next;
+};
+
 struct usb_change_list {
 	u_short vid[USB_DEVICE_UGEN_FORCE_MAX];
 	u_short pid[USB_DEVICE_UGEN_FORCE_MAX];
@@ -30,7 +38,7 @@ usb_list_ioctl(int f, struct usb_device_ugen_force *udf, int set)
 {
 	int e;
 
-	udf->udf_set = set;
+	udf->udf_cmd = set;
 	e = ioctl(f, USB_DEVICE_FORCE_UGEN, udf);
 	if (e)
 		err(EXIT_FAILURE, "ioctl()");
@@ -101,11 +109,71 @@ usb_list_change(struct usb_device_ugen_force *udf, struct usb_change_list *u)
 			}
 }
 
+struct usb_dev_list *
+usb_dev_get(void)
+{
+        struct usb_device_info inf;
+	struct usb_dev_list *ret;
+        struct usb_dev_list **p;
+        char buf[11];
+	int a, f, i;
+
+	ret = NULL;
+	p = &ret;
+	for (i = 0; i < 10; i++) {
+	        snprintf(buf, sizeof(buf), "/dev/usb%d", i);
+		f = open(buf, O_RDONLY);
+		if (f == -1)
+		        continue;
+		for (a = 1; a < USB_MAX_DEVICES; a++) {
+		        inf.udi_addr = a;
+			if (ioctl(f, USB_DEVICEINFO, &inf))
+			        continue;
+			*p = malloc(sizeof(**p));
+			if (!*p)
+			        err(EXIT_FAILURE, "malloc()");
+			(*p)->addr = a;
+			(*p)->ctrl = strdup(buf);
+			if (!((*p)->ctrl))
+              		        err(EXIT_FAILURE, "malloc()");
+			(*p)->vid = inf.udi_vendorNo;
+			(*p)->pid = inf.udi_productNo;
+			(*p)->next = NULL;
+			p = &((*p)->next);
+		}
+		close(f);
+	}
+	return (ret);
+}
+
+void
+usb_dev_reset(struct usb_change_list *c, struct usb_dev_list *l)
+{
+  	struct usb_device_ugen_force udf;
+	struct usb_dev_list *p;
+	int f, i;
+
+       	for (i = 0; i < c->end + 1; i++)
+	        for (p = l; p; p = p->next)
+		        if ((c->vid[i] == p->vid) && (c->pid[i] == p->pid)) {
+			        f = open(p->ctrl, O_RDONLY);
+				if (f == -1)
+				        err(EXIT_FAILURE,
+					    "can't open usb device %s",
+					    p->ctrl);
+				udf.udf_addr = p->addr;
+				usb_list_ioctl(f, &udf,
+					       USB_DEVICE_UGEN_FORCE_RESET);
+				close(f);
+			}
+}
+
 int
 main(int argc, char **argv)
 {
 	struct usb_device_ugen_force udf;
 	struct usb_change_list usb_change;
+	struct usb_dev_list *list;
 	int ch, f, i;
 	char *dev;
 
@@ -124,12 +192,12 @@ main(int argc, char **argv)
 			dev = optarg;
 			break;
 		case 'r':
-			udf.udf_set = 1;
+			udf.udf_cmd = USB_DEVICE_UGEN_FORCE_SET;
 			usb_list_mod(&usb_change, optarg, i, 1);
 			i++;
 			break;
 		case 's':
-			udf.udf_set = 1;
+		        udf.udf_cmd = USB_DEVICE_UGEN_FORCE_SET;
 			usb_list_mod(&usb_change, optarg, i, 0);
 			i++;
 			break;
@@ -147,13 +215,15 @@ main(int argc, char **argv)
 	if (f == -1)
 		err(EXIT_FAILURE, "can't open usb device %s", dev);
 
-	if (udf.udf_set) {
-		usb_list_ioctl(f, &udf, 0);
+	if (udf.udf_cmd == USB_DEVICE_UGEN_FORCE_SET) {
+                list = usb_dev_get();
+		usb_list_ioctl(f, &udf, USB_DEVICE_UGEN_FORCE_GET);
 		usb_list_change(&udf, &usb_change);
-		usb_list_ioctl(f, &udf, 1);
+		usb_list_ioctl(f, &udf, USB_DEVICE_UGEN_FORCE_SET);
+		usb_dev_reset(&usb_change, list);
 	}
 
-	usb_list_ioctl(f, &udf, 0);
+	usb_list_ioctl(f, &udf, USB_DEVICE_UGEN_FORCE_GET);
 	printf("currently ugen forced devices:\n");
 	for (i = 0; i < USB_DEVICE_UGEN_FORCE_MAX; i++)
 		if (udf.udf_ven_id[i] && udf.udf_dev_id[i])
